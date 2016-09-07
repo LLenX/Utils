442459449b0902dfdfd755173073811ccbdb0ddd
Alvin Xue <19786654@qq.com>
2016-09-07 13:56:22 +0800

    add support to more search

--- dict/Dictionary.py
+++ dict/Dictionary.py
@@ -5,14 +5,29 @@ import bs4
 
 class Dictionary(object):
 
-    def search(word):
-        soup = BeautifulSoup(urlopen("http://dict.cn/" + word), "lxml")
-        basicTag = soup.find_all("ul", class_="dict-basic-ul")[0]
+    def __init__(self, dictType="dict"):
+        if dictType == "dict":
+            self.url = "http://dict.cn/"
+            self.dictType = 0
 
-        result = ''
+    def search(self, word):
+        if self.dictType == 0:
+            return self.__searchDict(word)
+
+    def __searchDict(self, word):
+        soup = BeautifulSoup(urlopen(self.url + word), "lxml")
+        result = ""
+
+        tagList = soup.find_all("ul", class_="dict-basic-ul")
+        if not tagList:
+            tagList = soup.find_all("div", class_="basic clearfix")
+            basicTag = tagList[0].contents[1]
+        else:
+            basicTag = tagList[0]
 
         for child in basicTag.children:
-            if not isinstance(child, bs4.NavigableString) and not child.has_attr("style"):
+            if (not isinstance(child, bs4.NavigableString) and
+                    not child.has_attr("style")):
                 tmpList = []
                 for s in child.strings:
                     tmpList.append(str(s).replace('；', ", "))
